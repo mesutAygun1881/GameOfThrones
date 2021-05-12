34ea10a30ed38996aac81556810d86779800382b
mesut aygun <mesutaygun35@gmail.com>
2021-05-12 16:49:26 +0300

    first commit

---
+++ GameOfThrones/HouseModel.swift
@@ -0,0 +1,8 @@
+//
+//  HouseModel.swift
+//  GameOfThrones
+//
+//  Created by Mesut Aygün on 11.05.2021.
+//
+
+import Foundation
---
+++ GameOfThrones/Houses.swift
@@ -0,0 +1,8 @@
+//
+//  Houses.swift
+//  GameOfThrones
+//
+//  Created by Mesut Aygün on 11.05.2021.
+//
+
+import Foundation
